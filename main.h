b7089bb5d445bd2aad4503f186e814fced90404a
Bryan Clark <bryan.clark@email.wsu.edu>
2014-07-24 12:30:23 -0700

    Create main.h

---
+++ main.h
@@ -0,0 +1,10 @@
+//Programmer: Bryan K Clark
+//Email: bryan.clark@email.wsu.edu
+//Phone: (509)294-4161
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "ArraySorter.h"
+
+int* GenArr(int count);
